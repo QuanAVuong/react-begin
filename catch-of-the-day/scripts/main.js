eed7954f9c362058ba463cab66ef596be0319b0d
Quan <QuanAVuong@users.noreply.github.com>
2017-02-04 16:42:10 -0500

    get data from input, go to store

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -5,10 +5,16 @@ var ReactDOM = require("react-dom");
 var ReactRouter  = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
+// var Navigation = ReactRouter.Navigation; // .History instead
+var History = ReactRouter.History;
 
-// separate npm module: import code for HTML5's push state
+
+// separate npm module "history": import code for HTML5's push state
 var createBrowserHistory = require("history/lib/createBrowserHistory");
+
+// Helper functions
+var helpers = require("./helpers");
+
 
 /*
 	APP
@@ -78,12 +84,35 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
+	// create mixins: populate StorePicker class with methods we need eg. History
+	mixins: [History],
+	
+	// goToStore event handler:
+	goToStore: function(event) { // pass event to prevent default
+		event.preventDefault(); // else page reload after submitting, 
+		console.log('Store submitted'); // and log won't be seen
+
+		// GET DATA FROM INPUT (get value from the DOM)
+		// this = StorePicker component
+		// refs: referencing the node anywhere inside a component
+		// eg. <input type="text" value="obnoxious-worried-knives" data-reactid=".0.1">
+		console.log('storeId', this.refs.storeId)
+		var storeId = this.refs.storeId.value;
+		console.log('storeId value', storeId) // eg. obnoxious-worried-knives
+
+		// pushState not available => require a mixin eg. ReactRouter.History
+		this.history.pushState(null, "/store/" + storeId); 
+
+
+		// transition from StorePicker to App components
+	},
+
 	render: function() {
 		var name = "Q";
-		return (
-			<form className="store-selector">
+		return ( // listening for submit on form, since users may type, then enter
+			<form className="store-selector" onSubmit={this.goToStore} >
 				<h2>Please Enter A Store, {name}</h2>
-				<input type="text" ref="storeId" />
+				<input type="text" ref="storeId" defaultValue={helpers.getFunName()} />
 				<input type="Submit" />
 			</form>
 		)
