3e3f3a8effa9ab23db1429ce9da81a8dbfcc8fba
Quan <QuanAVuong@users.noreply.github.com>
2017-02-05 10:31:57 -0500

    add fish from <AddFishForm /> to <App />

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -16,11 +16,29 @@ var createBrowserHistory = require("history/lib/createBrowserHistory");
 var helpers = require("./helpers");
 
 
-/*
-	APP
-*/
+/******************************************
+					APP
+*******************************************/
 
 var App = React.createClass({
+	getInitialState: function() {
+		return {
+			fishes: {},
+			order: {},
+		}
+	},
+
+	// addFish method here instead of in <AddFishForm />, to be passed down
+	addFish: function(fish) {
+		var timestamp = (new Date()).getTime(); // give fishes unique IDs
+		this.state.fishes["fish-" + timestamp] = fish; // update state object
+
+		// actually set state: pass object with new state
+		// NOT (this.state): pass the entire state is slow
+		// be specific, keep amount of comparisons as few as possible
+		this.setState({ fishes: this.state.fishes }); 
+	},
+
 	render: function() {
 		return (
 			<div className="catch-of-the-day">
@@ -28,11 +46,53 @@ var App = React.createClass({
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish}/>
 			</div>
 		)
 	}
 })
+
+
+/*
+	Add Fish Form <AddFishForm />
+ */
+
+var AddFishForm = React.createClass({
+	createFish: function(event) {
+		// 1. Stop the form from submitting
+		event.preventDefault();
+		// 2. Take data from form => create object
+		var fish = {
+			name: this.refs.name.value,
+			price: this.refs.price.value,
+			status: this.refs.status.value,
+			desc: this.refs.desc.value,
+			image: this.refs.image.value,
+		}
+		console.log(fish);
+		// 3. Add fish to App State
+		// App.addFish(fish) would makes sense, but addFish isn't in AddFishForm
+		this.props.addFish(fish); // passed down from <Inventory addFish={...this.props}/>
+		
+	},
+
+	render : function() {
+	  return ( // ref referencing corresponding nodes
+	    <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+	      <input type="text" ref="name" placeholder="Fish Name"/>
+	      <input type="text" ref="price" placeholder="Fish Price" />
+	      <select ref="status">
+	        <option value="available">Fresh!</option>
+	        <option value="unavailable">Sold Out!</option>
+	      </select>
+	      <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+	      <input type="text" ref="image" placeholder="URL to Image" />
+	      <button type="submit">+ Add Item </button>
+	    </form>
+	  )
+	}
+})
+
 
 /*
 	Header
@@ -73,7 +133,10 @@ var Order = React.createClass({
 var Inventory = React.createClass({
 	render: function() {
 		return (
-			<p>Inventory</p>
+			<div>
+				<h2>Inventory</h2>
+				<AddFishForm {...this.props} />
+			</div>
 		)
 	}
 })
@@ -86,7 +149,7 @@ var Inventory = React.createClass({
 var StorePicker = React.createClass({
 	// create mixins: populate StorePicker class with methods we need eg. History
 	mixins: [History],
-	
+
 	// goToStore event handler:
 	goToStore: function(event) { // pass event to prevent default
 		event.preventDefault(); // else page reload after submitting, 
